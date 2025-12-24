76f3efafe6c91b632908697e0f3f7a81176fe5c7
Usama Bin Faheem <usamaformal@gmail.com>
2025-12-23 22:46:50 -0600

    Time: 86 ms (27.75%) | Memory: 22.7 MB (48.72%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -7,12 +7,12 @@ class Solution:
             dist = x*x + y*y
             heap.append ([dist,x,y])
         heapq.heapify(heap)
-        
         while k > 0 :
-            res.append(heapq.heappop(heap))
+            dist, x, y = (heapq.heappop(heap))
             k = k-1
-        for dist , x, y in res : 
-            ans.append([x,y])
-        return ans 
+            res.append([x,y])
+        # for dist,x, y in res : 
+        #     ans.append([x,y])
+        return res 
         
         
