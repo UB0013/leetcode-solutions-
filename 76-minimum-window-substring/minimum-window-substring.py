270b8e3d86ca50457f373d581f0ed6f7b8b87fd2
Usama Bin Faheem <usamaformal@gmail.com>
2025-12-27 10:53:33 -0600

    Time: 130 ms (31.80%) | Memory: 18.1 MB (84.77%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,35 +1,29 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        countt = {}
         counts = {}
+        countt = {}
+
+        l, r  = 0 , 0 
+        reslen  =  float("inf")
+        have = 0 
+        res = [-1,1]
 
         for c in t : 
-            countt[c] = 1+ countt.get(c,0)
-
+            countt[c] = 1 + countt.get (c,0) 
         need = len (countt)
-        reslen = float("inf")
 
-        l = 0 
-        r = 0 
-        have = 0 
-
-        for r in range (len(s)):
+        while r < len(s) :
             counts[s[r]] = 1 + counts.get(s[r],0)
-            if s[r] in countt and countt[s[r]] == counts[s[r]]:
-                have += 1  
-            while have == need :
+            if s[r] in countt and counts[s[r]] == countt[s[r]]:
+                have = have + 1 
+            while have == need : 
                 if r-l+1 < reslen : 
                     res = s[l:r+1]
-                    reslen = len(res) 
-                counts[s[l]] -= 1 
-                if  s[l] in  countt and counts[s[l]] <  countt[s[l]]  : 
-                        have -= 1 
-                l = l+1
-        
-        return res if reslen !=  float("inf") else  ""
+                    reslen = r-l+1 
+                counts[s[l]]  -= 1 
+                if s[l] in countt and counts[s[l]] < countt[s[l]] : 
+                    have -= 1 
+                l = l+1 
+            r = r + 1 
+        return res if reslen !=  float("inf") else ""
 
-                
-
-
-
-         
