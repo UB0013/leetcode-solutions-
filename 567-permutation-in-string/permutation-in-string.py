91f169e0e07ac0c6aaf88390613703c29342be21
Usama Bin Faheem <usamaformal@gmail.com>
2025-12-27 09:48:04 -0600

    Time: 15 ms (84.81%) | Memory: 17.6 MB (87.18%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,30 +1,20 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq1 = [0]*26
+        freq2 = [0]*26
 
-        freqs1 ={}
-        freqs2 ={}
         for c in s1 :
-            freqs1[c] = 1+ freqs1.get(c, 0)
+            freq1[ord(c)- ord("a")] +=1  
 
-        l,r = 0, 0 
+        l , r = 0, 0 
 
-        while r < (len(s2)) : 
-            freqs2[s2[r]] = 1+ freqs2.get(s2[r], 0 )
-            if r-l+1 > len(s1):
-                freqs2[s2[l]] -= 1
-                #we will have to delete zero frequecies if we sue a dictionary -- not required if using array of 26 
-                if freqs2[s2[l]] == 0:
-                    del freqs2[s2[l]]
-                l= l+1 
-
-            if freqs1 ==freqs2:
-                return True 
-            r = r + 1 
-        return False
-
-
-
-
-
-
-        
+        for r in range (len(s2)):
+            freq2[ord(s2[r])- ord("a")] += 1 
+            if r-l+1 > len(s1): 
+                freq2[ord(s2[l]) - ord("a")] -= 1
+                l = l+1
+            if freq1 ==freq2 :
+                return True
+            r = r+1
+        return False 
+         
