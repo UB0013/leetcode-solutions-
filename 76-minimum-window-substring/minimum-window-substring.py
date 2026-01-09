273e67fc41bd47bf6634d7b4b3e557f177487552
Usama Bin Faheem <usamaformal@gmail.com>
2026-01-09 07:01:53 -0600

    Time: 119 ms (36.80%) | Memory: 19.5 MB (5.01%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.py
+++ 76-minimum-window-substring/minimum-window-substring.py
@@ -1,29 +1,32 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        counts = {}
         countt = {}
+        counts = {}
+        have =0 
+        for c in t :
+            countt[c] = 1 + countt.get(c,0)
+        need= len (countt)
 
-        l, r  = 0 , 0 
-        reslen  =  float("inf")
-        have = 0 
-        res = [-1,1]
+        l =0 
+        r= 0 
+        res = float("infinity")
+        val = [-1,1]
 
-        for c in t : 
-            countt[c] = 1 + countt.get (c,0) 
-        need = len (countt)
+        for r in range (len(s)):
+            c= s[r]
+            counts[c] = 1 + counts.get(c,0)
+            if c in countt and countt[c]==counts[c]:
+                have = have+1
+            while need == have : 
+                if r-l+1 < res :
+                    res = r-l+1
+                    val = s[l:r+1]
+                counts[s[l]] = counts[s[l]]-1
+                if s[l] in countt and counts[ s[l] ] < countt [s[l]]:
+                    have = have-1
+                l = l + 1 
+        return val if res != float("infinity") else ""    
 
-        while r < len(s) :
-            counts[s[r]] = 1 + counts.get(s[r],0)
-            if s[r] in countt and counts[s[r]] == countt[s[r]]:
-                have = have + 1 
-            while have == need : 
-                if r-l+1 < reslen : 
-                    res = s[l:r+1]
-                    reslen = r-l+1 
-                counts[s[l]]  -= 1 
-                if s[l] in countt and counts[s[l]] < countt[s[l]] : 
-                    have -= 1 
-                l = l+1 
-            r = r + 1 
-        return res if reslen !=  float("inf") else ""
-
+  
+        
+        
