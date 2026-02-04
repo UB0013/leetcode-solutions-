650f72a4f5ef48d37c3bf3391d18ad587da24401
Usama Bin Faheem <usamaformal@gmail.com>
2026-02-04 06:39:05 -0600

    Time: 25 ms (82.89%) | Memory: 20.9 MB (34.31%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,46 +1,52 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        rows = len(heights)
-        cols = len(heights[0])
+        rows = len (heights)
+        cols = len (heights[0])
+        atlanticvisit = [[False] * cols for _ in range (rows) ]
+        pacificvisit = [[False] * cols for _ in range (rows) ]
         pacific = []
         atlantic = []
-        visitedpacific = [[False] * cols for  n in range(rows)]
-        visitedatlantic = [[False]* cols for n in range(rows)]
-        directions = [(1,0),(-1,0),(0,1),(0,-1)]
-
-        for r in range(rows):
-            pacific.append((r,0))
+        res = [] 
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        for r in range (rows)  :
+            pacific.append ((r,0))
             atlantic.append((r,cols-1))
-        for c in range(cols):
+            pacificvisit[r][0] = True
+            atlanticvisit[r][cols-1] = True 
+        for c in range (cols):
             pacific.append((0,c))
-            atlantic.append((rows-1,c))
+            atlantic.append ((rows-1,c)) 
+            pacificvisit[0][c] = True
+            atlanticvisit [rows-1][c] = True 
 
-        def bfs (ocean,visit):
-            q = deque(ocean)
-
+        def bfs (ocean,visit) :
+            q = deque (ocean)
             while q : 
-                r, c = q.popleft()
-                visit[r][c] = True 
-                for mr , mc in directions:
-                    nr = r+ mr 
-                    nc = c + mc
-                    if nr < 0 or nc<0 or nr >= rows or nc >= cols or heights[nr][nc] < heights[r][c] or visit[nr][nc] == True  :
-                        continue
+                r, c = q.popleft ()
+                for mr , mc in directions :
+                    nr = r + mr 
+                    nc = c + mc 
+                    if nr <0 or nc < 0 or nr >= rows or nc >= cols or heights[nr][nc] < heights[r][c] or visit[nr][nc]  :
+                        continue 
                     q.append((nr,nc))
+                    visit[nr][nc] = True 
+            
 
 
+        bfs (pacific, pacificvisit)
+        bfs (atlantic,atlanticvisit)
 
-        bfs(pacific,visitedpacific)
-        bfs (atlantic,visitedatlantic)
 
-        res =[]
+        
 
-        for r in range(rows):
-            for c in range(cols):
-                if visitedpacific[r][c] and visitedatlantic[r][c]:
-                    res.append((r,c))
-
+        for r in range (rows) :
+            for c in range (cols) : 
+                if pacificvisit[r][c] and atlanticvisit[r][c] : 
+                    res.append ((r,c))
         return res 
 
+          
 
+    
+            
         
