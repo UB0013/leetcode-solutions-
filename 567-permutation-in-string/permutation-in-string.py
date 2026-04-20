ce24b3b88eea6607da59e6286a3c8f0fefac7aba
Usama Bin Faheem <usamaformal@gmail.com>
2026-04-20 02:41:25 -0500

    Time: 19 ms (61.65%) | Memory: 19.2 MB (98.15%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.py
+++ 567-permutation-in-string/permutation-in-string.py
@@ -1,20 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        freq1 = [0]*26
-        freq2 = [0]*26
 
-        for c in s1 :
-            freq1[ord(c)- ord("a")] +=1  
+        freq1 = [0]*26 
+        freq2 = [0]*26 
 
-        l , r = 0, 0 
-
-        for r in range (len(s2)):
-            freq2[ord(s2[r])- ord("a")] += 1 
+        for c in s1 : 
+            freq1[ord('a')- ord(c)] += 1 
+        l = 0 
+        r = 0 
+        for r in range (len(s2)): 
+            freq2[ord('a') - ord(s2[r])] += 1  
             if r-l+1 > len(s1): 
-                freq2[ord(s2[l]) - ord("a")] -= 1
-                l = l+1
-            if freq1 ==freq2 :
-                return True
-            r = r+1
-        return False 
-         
+                freq2[ord('a') - ord(s2[l])] -= 1  
+                l = l +1 
+            if freq1 == freq2 :
+                return True 
+        return False
