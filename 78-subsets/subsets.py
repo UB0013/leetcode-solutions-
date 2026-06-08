91b84942d607fc568f41c99b648f1db8ca22cd90
Usama Bin Faheem <usamaformal@gmail.com>
2026-06-07 19:01:45 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (5.55%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,17 +1,20 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = []
-        stack = []
+        subset = [ ]
+        result = [] 
+        n = len (nums)
 
-        i=0 
-
-        def dfs (stack, i):
-            if i == len (nums) :
-                res.append(stack.copy())
+        def dfs (i) : 
+            if i ==  n :
+                result.append(subset.copy())
                 return 
-            stack.append(nums[i])
-            dfs(stack,i+1)
-            stack.pop()
-            dfs (stack,i+1)
-        dfs (stack,0)
-        return res 
+            subset.append(nums[i])
+            dfs (i+1)
+            subset.pop()
+            dfs (i+1)
+
+        dfs (0)
+        return result 
+
+
+        
